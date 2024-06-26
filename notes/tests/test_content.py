ee2c73f0f8f0b42afb0884a05825c50629c8d516
Vladislav Sirotkin <vladislav.sirotkin@jti.com>
2024-06-26 12:04:40 +0300

    add content tests

---
+++ notes/tests/test_content.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from notes.forms import NoteForm
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.note = Note.objects.create(
+            title='Заметка новость',
+            text='Просто текст.',
+            slug='notton',
+            author=cls.author,
+        )
+        # Сохраняем в переменную адрес страницы с новостью:
+        cls.add_url = reverse('notes:add')
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+
+    def test_authorized_client_has_form(self):
+        """Тестируем наличие формы создания и редактирования"""
+        self.client.force_login(self.author)
+        for url in (self.add_url, self.edit_url):
+            response = self.client.get(url)
+            self.assertIn('form', response.context)
+            self.assertIsInstance(response.context['form'], NoteForm)
